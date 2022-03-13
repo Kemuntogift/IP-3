754d597c8fd606556c3960c5ebf302a47ae34157
Kemunto Gift <giftoyugi@gmail.com>
2022-03-13 19:42:47 +0300

    toggle design icon to hide and show text

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 $(document).ready(function() {
-            //form validation
-            $("#form").submit(function(event) {
-                let userName = $("input#name").val();
-                let userMail = $("input#email").val();
-                let userMessage = $("input#message").val();
-                if (userName != "" && userMail != "" && userMessage != "") {
-                    alert(
-                        "Hello " +
-                        userName +
-                        ". Thank you for reaching out to us. We have received your feedback and will get back to you shortly.");
-                } else {
-                    alert("Invalid input! Please fill in your name and email address and leave a meesage.");
-                }
-                event.preventDefault();
-            });
+    //form validation
+    $("#form").submit(function(event) {
+        let userName = $("input#name").val();
+        let userMail = $("input#email").val();
+        let userMessage = $("input#message").val();
+        if (userName != "" && userMail != "" && userMessage != "") {
+            alert(
+                "Hello " +
+                userName +
+                ". Thank you for reaching out to us. We have received your feedback and will get back to you shortly."
+            );
+        } else {
+            alert(
+                "Invalid input! Please fill in your name and email address and leave a meesage."
+            );
+        }
+        event.preventDefault();
+    });
 
-            //add hover effect on portfolio images
-            $('.portfolioimg').hover(function() {
-                $('.portfoliotext', this).slideToggle('slow');
-            }, function() {
-                $('.porttext', this).slideToggle('slow');
-            });
+    //add hover effect on portfolio images
+    $(".portfolioimg").hover(
+        function() {
+            $(".portfoliotext", this).slideToggle("slow");
+        },
+        function() {
+            $(".portfoliotext", this).slideToggle("slow");
+        });
+
+
+    //toggle
+
+    $(".one").click(function() {
+        $(".one").fadeIn("slow").hide();
+        $("#design").fadeIn().hide();
+        $(".service1").show();
+    });
+    $(".service1").click(function() {
+        $(".service1").slideUp();
+        $(".one").slideDown();
+        $("#design").slideDown();
+    });
+});
