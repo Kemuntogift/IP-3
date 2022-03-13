95813e9c8380fd0bd08ecae9b0a7d2f119dee846
Kemunto Gift <giftoyugi@gmail.com>
2022-03-13 17:59:37 +0300

    link script.js file and jQuery CDN to index.html file

---
+++ script.js
@@ -0,0 +1,5 @@
+//form data validation
+let userForm = document.getElementById("form");
+let userName = document.getElementById("name");
+let userMail = document.getElementById("email");
+let userMessage = document.getElementById("message");
