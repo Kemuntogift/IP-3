8f78cf1ac834c37ac71b5b9afd1715a462f63cff
Kemunto Gift <giftoyugi@gmail.com>
2022-03-13 18:31:52 +0300

    add if else function for message alert

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ $(document).ready(function() {
             $("#form").submit(function(event) {
                         let userName = $("input#name").val();
                         let userMail = $("input#email").val();
-                        let userMessage = $("input#message").val(
+                        let userMessage = $("input#message").val();
+                        if (userName != "" && userMail != "" && userMessage != "") {
+                            alert(
+                                "Hello " +
+                                userName +
+                                ". Thank you for reaching out to us. We have received your feedback and will get back to you shortly.");
+                        } else {
+                            alert("Invalid input! Please fill in your name and email address and leave a meesage.");
+                        }
